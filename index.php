b2f7626cb98ad22bb76ce7c72766151471ce57ff
root <emploi.neveu@gmail.com>
2018-11-14 16:03:39 +0100

    exo1

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+  </head>
+  <body>
+    <?php
+    $name = "Rico";
+    echo "Bonjour $name";
+    ?>
+  </body>
+</html>
